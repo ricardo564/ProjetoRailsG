ad6d5cd996a3200b13b68bf6283925b257ade12d
Ricardo564 <ricardo_hks@hotmail.com>
2019-11-13 08:58:56 -0300

    primeiro commit

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_many :ideas
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include Gravtastic
+  gravtastic
+end
